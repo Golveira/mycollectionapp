880d72f1c99e2654bb4eb22f34d15b2eaf933a81
Golveira <gabriel.golveira@gmail.com>
2019-03-04 11:38:34 -0300

    Update Settings

--- mycollection/settings.py
+++ mycollection/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n-6+e6_6z(h0gl(!x)&nlhm9ds)-!admxi1g6oz-o_5q5airgt'
+# SECRET_KEY = 'n-6+e6_6z(h0gl(!x)&nlhm9ds)-!admxi1g6oz-o_5q5airgt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mycollectionapp.herokuapp.com']
+ALLOWED_HOSTS = ['mycollectionapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
