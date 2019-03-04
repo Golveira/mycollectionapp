a8f394934d033f910dae475cc3c446a85c6fe64e
Golveira <gabriel.golveira@gmail.com>
2019-03-04 11:22:17 -0300

    Add AWS Settings

--- mycollection/settings.py
+++ mycollection/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n-6+e6_6z(h0gl(!x)&nlhm9ds)-!admxi1g6oz-o_5q5airgt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mycollectionapp.herokuapp.com']
 
 
 # Application definition
@@ -142,3 +142,11 @@ LOGOUT_REDIRECT_URL = 'login'
 
 LOGIN_URL = 'login'
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
